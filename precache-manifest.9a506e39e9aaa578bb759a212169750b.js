5f3b8cb81118b8ff1c28735992c4f1ac853841c2
Jeff <jeffosten@gmail.com>
2019-02-05 17:00:22 +0100

    Updates

---
+++ precache-manifest.9a506e39e9aaa578bb759a212169750b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fda731ddf47be4ffc6ffbf40a0635e23",
+    "url": "/allocine/static/media/allocine_logo.fda731dd.svg"
+  },
+  {
+    "revision": "8fbe013908822a06568b",
+    "url": "/allocine/static/js/runtime~main.8fbe0139.js"
+  },
+  {
+    "revision": "65fc9245dd69968df33d",
+    "url": "/allocine/static/js/main.65fc9245.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/allocine/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "65fc9245dd69968df33d",
+    "url": "/allocine/static/css/main.c7abde1a.chunk.css"
+  },
+  {
+    "revision": "c1f5f6806d343aff79db45e1d4069b66",
+    "url": "/allocine/index.html"
+  }
+];
